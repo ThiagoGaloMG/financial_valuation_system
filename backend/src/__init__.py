ec8822a824f188c0772a82c11bb5099eab775f56
ThiagoGaloMG <91726223+ThiagoGaloMG@users.noreply.github.com>
2025-06-17 22:51:55 -0300

    Update __init__.py

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,6 @@
 # backend/src/__init__.py
-
 # Este arquivo torna o diretório 'src' um pacote Python.
 # Pode ser usado para inicializações de pacotes ou configurações globais.
 
-self.request_delay = 1.5
+YFINANCE_REQUEST_DELAY = 1.5
+REQUEST_TIMEOUT = 10
